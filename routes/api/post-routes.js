ba40da2ad69de08508fe037b14eaf68a470679a7
Blake Johnson <blakejohnson@Blakes-MBP.attlocal.net>
2021-05-11 14:43:04 -0500

    comment model and routes finfished

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,31 +1,35 @@
 const router = require("express").Router();
-const { Post, User, Vote } = require("../../models");
+const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
 
 // get all posts
 router.get("/", (req, res) => {
   console.log("======================");
   Post.findAll({
+    order: [['created_at', 'DESC']],
     attributes: [
-      "id",
-      "post_url",
-      "title",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-    order: [["created_at", "DESC"]],
     include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
+        attributes: ['username']
+      }
+    ]
+   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
       console.log(err);
@@ -56,6 +60,14 @@ router.get("/:id", (req, res) => {
         model: User,
         attributes: ["username"],
       },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      }
     ],
   })
     .then((dbPostData) => {
@@ -90,6 +102,7 @@ router.post("/", (req, res) => {
 router.put("/upvote", (req, res) => {
   // custom static method created in models/Post.js
   Post.upvote(req.body, { Vote })
+
     .then((updatedPostData) => res.json(updatedPostData))
     .catch((err) => {
       console.log(err);
